2c1023c7d1c979f0a2bd83369aae370ab50d35fd
A80314 <itriA80314@itri.org.tw>
2021-06-11 08:28:04 +0800

    new file:   dist/Azure_IoT_Methods.js 	new file:   dist/REDEYE_New_Data.js 	modified:   src/Azure_IoT_Device.ts 	new file:   src/Azure_IoT_Methods.ts 	new file:   src/REDEYE_New_Data.ts

---
+++ dist/REDEYE_New_Data.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class REDEYE_New_Data {
+    constructor() {
+        this.user_id = "";
+        this.data_value = "";
+        this.create_datetime = "";
+        this.spectrum = new Array();
+    }
+}
+exports.REDEYE_New_Data = REDEYE_New_Data;
+class REDEYE_Raw_Data {
+    constructor() {
+        this.wavelength = 0;
+        this.blank = 0;
+        this.sample = 0;
+    }
+}
+exports.REDEYE_Raw_Data = REDEYE_Raw_Data;
